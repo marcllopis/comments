94b33dba4d310bd811b19d02c5ea13e2e473b702
Marc <marcllopis89@gmail.com>
2022-03-17 14:32:37 +0100

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let landingSection = document.getElementById("landing");
 let mainSection = document.getElementById("main-page");
 
 // button to control how to get to the comments section
-enterButton.addEventListener("click", () => {
+enterButton.addEventListener("click", function () {
   landingSection.classList.add("hide");
   mainSection.classList.remove("hide");
   mainSection.classList.add("show");
@@ -15,9 +15,11 @@ let input = document.getElementById("user");
 let text = document.getElementById("text");
 let commentsSection = document.getElementById("comments");
 
-// function in charge of generating the needed html to append a comment with it's username and it's text
+// control the click on the comment button
+button.addEventListener("click", function () {
+  let inputValue = input.value;
+  let textValue = text.value;
 
-const generateHtmlComment = (nameOfTheUser, commentFromUser) => {
   // create all html needed for a comment
   let commentContainer = document.createElement("div");
   let spaceBetweenLines = document.createElement("br");
@@ -26,8 +28,8 @@ const generateHtmlComment = (nameOfTheUser, commentFromUser) => {
   let separator = document.createElement("hr");
 
   // Mix the user name and comment with the created html elements
-  userNameElement.innerHTML = `${nameOfTheUser} said:`;
-  userCommentElement.innerHTML = commentFromUser;
+  userNameElement.innerHTML = `${inputValue} said:`;
+  userCommentElement.innerHTML = textValue;
 
   // add all those elements to the div container
   commentContainer.appendChild(spaceBetweenLines);
@@ -37,14 +39,7 @@ const generateHtmlComment = (nameOfTheUser, commentFromUser) => {
 
   // add the comment div (with all its childs) to the html
   commentsSection.appendChild(commentContainer);
-};
 
-// control the click on the comment button
-button.addEventListener("click", () => {
-  let inputValue = input.value;
-  let textValue = text.value;
-  // calls the fn in charge of creating the needed html with the needed arguments
-  generateHtmlComment(inputValue, textValue);
   // clear input and textarea fields
   input.value = "";
   text.value = "";
@@ -65,8 +60,27 @@ let listOfComments = [
   },
 ];
 
-// iterates over the initial array that contains the comments from different users we want to show when the page loads
+for (let i = 0; i < listOfComments.length; i++) {
+  let user = listOfComments[i].user;
+  let comment = listOfComments[i].comment;
 
-listOfComments.forEach((element) =>
-  generateHtmlComment(element.user, element.comment)
-);
+  // create all html needed for a comment
+  let commentContainer = document.createElement("div");
+  let spaceBetweenLines = document.createElement("br");
+  let userNameElement = document.createElement("h4");
+  let userCommentElement = document.createElement("p");
+  let separator = document.createElement("hr");
+
+  // Mix the user name and comment with the created html elements
+  userNameElement.innerHTML = `${user} said:`;
+  userCommentElement.innerHTML = comment;
+
+  // add all those elements to the div container
+  commentContainer.appendChild(spaceBetweenLines);
+  commentContainer.appendChild(userNameElement);
+  commentContainer.appendChild(userCommentElement);
+  commentContainer.appendChild(separator);
+
+  // add the comment div (with all its childs) to the html
+  commentsSection.appendChild(commentContainer);
+}
